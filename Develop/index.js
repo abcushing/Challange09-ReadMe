48e911249e411075db6b145b40e4b0a00a7821ef
abcushing <abcushing789@gmail.com>
2022-11-14 09:09:47 -0700

    2nd-commit
    2nd commit, added questions and generate markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require(./utils/generateMarkdown);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type:'input',
+    name:'github',
+    message:'What is your Github username?',
+},
+
+{
+    type:'input',
+    name:'email',
+    message:'What is your email address?',
+},
+
+{
+    type:'input',
+    name:'title',
+    message:'What is the name of the project'
+},
+
+{
+    type:'input',
+    name:'description',
+    message:'Write a short description of the project',
+},
+
+{
+    type:'input',
+    name:'license',
+    message:'What kind of licnce will you use? (n/a for none)',
+    choices: ['MIT', 'APACHE 2.0','GPL3.0','BSD 3', 'n/a' ],
+},
+
+{
+    type:'input',
+    name:'installation',
+    message:'What command should be run to install dependancies? (npm i is the default)',
+    default: 'npm i',
+},
+
+{
+    type:'input',
+    name:'testing',
+    message:'What command to run tests (default is npm test)',
+    default:'npm test',
+},
+
+{
+    type:'input',
+    name:'useage',
+    message:'What does the user need to know about using the repository?',
+},
+
+{
+    type:'input',
+    name:'contributing',
+    message:'what does the user need to know about contributing to this project?'
+},
+
+{
+    type:'input',
+    name:'notes',
+    message:'Any other notes about this project?'
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('generate README...');
+        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else if (license === 'ISC') {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  } else if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license === 'GNU GPL v3') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return ``;
+  } else if (license === 'ISC') {
+    return `https://choosealicense.com/licenses/isc/`
+  } else if (license === 'MIT') {
+    return `https://choosealicense.com/licenses/mit/`
+  } else if (license === 'GNU GPL v3') {
+    return `https://choosealicense.com/licenses/gpl-3.0/`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `This project is under the the ${license} license.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [License](#license)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  ## License
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+  ## Test
+  ${data.test}
+  ## Questions
+  * [Github](https://github.com/${data.account})
+  * I am reachable at ${data.email} for any additonal questions you may have.
 `;
 }
 
