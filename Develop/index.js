599e5a896fa9253cadf589b50a419656b36683b0
abcushing <abcushing789@gmail.com>
2022-11-17 09:07:38 -0700

    9th-commit
    9th commit- added more comments, fixed some spelling. Hopefully that is enough commits, not sure what else to do

--- Develop/index.js
+++ Develop/index.js
@@ -5,37 +5,43 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // An array of questions for user input
 const questions = [
-{
+// this question is for Github username input
+    {
     type:'input',
     name:'github',
     message:'What is your Github username?',
 },
 
+// this question is for email address input
 {
     type:'input',
     name:'email',
     message:'What is your email address?',
 },
 
+// this question is for project name input
 {
     type:'input',
     name:'title',
     message:'What is the name of the project?'
 },
 
+// this question is for project description input
 {
     type:'input',
     name:'description',
     message:'Write a short description of the project',
 },
 
+// this question is for selecting license input
 {
     type:'input',
     name:'license',
-    message:'What kind of licnce will you use? (n/a for none)',
+    message:'What kind of license will you use? (n/a for none)',
     choices: ['MIT', 'APACHE 2.0','GPL3.0','BSD 3', 'n/a' ],
 },
 
+// this question is for selecting install command input
 {
     type:'input',
     name:'installation',
@@ -43,6 +49,7 @@ const questions = [
     default: 'npm i',
 },
 
+// this question is for selecting which command to run tests input
 {
     type:'input',
     name:'testing',
@@ -50,18 +57,21 @@ const questions = [
     default:'npm test',
 },
 
+// this question is for repo description/use input
 {
     type:'input',
     name:'useage',
     message:'What does the user need to know about using the repository?',
 },
 
+// this question is for project contribution description/use input
 {
     type:'input',
     name:'contributing',
     message:'What does the user need to know about contributing to this project?'
 },
 
+// this question is for general notes about the project input
 {
     type:'input',
     name:'notes',
