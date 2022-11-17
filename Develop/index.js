6bee96d9d7497f57a821ef6f8e485bdb7df6bcf9
abcushing <abcushing789@gmail.com>
2022-11-17 08:45:08 -0700

    6th-commit
    6th commit- edited comments based on feedback from grader ID

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
 {
     type:'input',
@@ -69,7 +69,7 @@ const questions = [
 },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if (err) {
@@ -79,7 +79,7 @@ function writeToFile(fileName, data) {
       });
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer.prompt(questions).then((res) => {
         console.log(res);
